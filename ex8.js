164e0db7035d60952c2cfdbfe6c30ccde58b9b5e
SreeHarsha-07 <iam.harshagowda@gmail.com>
2022-11-27 12:01:43 +0530

    third

--- ex8.js
+++ ex8.js
@@ -1,40 +1,14 @@
-function rect(value){
-    var l = 0;
-    for(var i=0;i<value.length;i++){  
-        var temp =value[i];
-        if(l<temp.length){
-            l=temp.length;     /* assigning length of the longest string in the list */
-        }
-    }
-    for(var j=0;j<l+6;j++)
-    {
-        process.stdout.write("*");
-    }
-    process.stdout.write("\n");
-    for(var i=0;i<value.length;i++)
-    {
-        var temp =value[i]
-       if(l==temp.length)
-       {
-        process.stdout.write("*  "+temp+"  *");
-       }
-       else
-       {
-        process.stdout.write("*  "+temp);
-        for(var j=temp.length;j<l;j++)
-        {
-            process.stdout.write(" ");
-        }
-        process.stdout.write("  *");
-       }
-       process.stdout.write("\n");
-    }
+var text = "The quick brown fox";
+var myArray = text.split(" ");
 
-    for(var j=0;j<l+6;j++)
-    {
-        process.stdout.write("*");
-    }
+function pig(myArray){
+  var neww=[];
+    let firstletter=myArray.charAt(0);
+    
+    neww=myArray.slice(1);
+    neww=neww+firstletter+"ay";
+    return neww;
+
 }
 
-var arr =["hello","world","in","a","frame"];
-rect(arr);
+console.log(myArray.map(pig));
